c028385ed7dfd883a4e381fa84732ccdd3d6682f
JRU-Mentor-Tatiana-Bakach <jru.mentor.tatianabakach@gmail.com>
2023-11-24 16:53:51 +0300

    Initial commit

---
+++ src/main/java/com/example/snakereactivemod5lec13/SnakeReactiveMod5Lec13Application.java
@@ -0,0 +1,13 @@
+package com.example.snakereactivemod5lec13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeReactiveMod5Lec13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakeReactiveMod5Lec13Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snakereactivemod5lec13/SnakeReactiveMod5Lec13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snakereactivemod5lec13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeReactiveMod5Lec13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
